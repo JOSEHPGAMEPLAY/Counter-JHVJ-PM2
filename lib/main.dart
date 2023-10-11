1222d50387a2cdc214fcf20d5215f51968f868a6
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-10-10 19:56:39 -0500

    Agregamos el Scalffold

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: Scaffold(
+      )
     );
   }
 }
