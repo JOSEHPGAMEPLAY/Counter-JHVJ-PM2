f1e921ae212064a8ddd676873952fc3c81ab28bc
JOSEHPGAMEPLAY <jose.kj11@hotmail.com>
2023-10-10 23:00:30 -0500

    Creacion de la tercera card

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,25 @@ class _MyAppState extends State<MyApp> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                
+                Card(
+                      semanticContainer: true,
+                      child: Column(
+                        children: [
+                          Text(
+                            '$three',
+                            style: const TextStyle(fontSize: 150),
+                          ),
+                          FloatingActionButton.extended(
+                            backgroundColor: Colors.teal,
+                            onPressed: () {
+                              setState(() => three++);
+                            },
+                            label: const Text('Aumentar'),
+                          ),
+                          const Padding(padding: EdgeInsets.all(10))
+                        ],
+                      ),
+                    ),
               ],)
           ],
         ),
